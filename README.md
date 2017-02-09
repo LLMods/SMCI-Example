ccba018b2a6f981d5d34c0486bad021b18a2bdbc
Andre LaFleur <andre.lafleur@utah.edu>
2017-02-09 09:51:44 -0700

    Add simple main class and README

---
+++ README.md
@@ -0,0 +1,11 @@
+# SMCI Example
+
+This is an example plugin to show how to create and build a Spigot mod for my dev server.
+
+## How to do
+
+1. Follow [this tutorial](https://www.spigotmc.org/wiki/creating-a-blank-spigot-plugin-in-intellijidea/) to get things setup.
+2. Make sure [Apache Maven](https://maven.apache.org/) is installed
+3. Create a pom.xml file like the one in this repository for Maven
+4. Write your mod and push it
+5. The SMCI server should do the rest
--- src/main/java/org/smci/example/Main.java
+++ src/main/java/org/smci/example/Main.java
@@ -2,10 +2,21 @@ package org.smci.example;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Created by tsvetok on 2/9/17.
  */
 public class Main extends JavaPlugin {
 
 
+    @Override
+    public void onEnable(){
+        Logger.getLogger("minecraft").log(Level.INFO, "[SMCI-Example] Hello, world!");
+    }
+
+    public void onDisable(){
+        Logger.getLogger("minecraft").log(Level.INFO, "[INFO] Goodbye, world!");
+    }
 }
